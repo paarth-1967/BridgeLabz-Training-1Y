195f806b34fef5cbb028828fa234be72625e000c
paarth-1967 <paarth30072006@gmail.com>
2026-01-12 21:27:14 +0530

    added: VolumeOfCylinder programming-constructs

---
+++ VolumeOfCylinder.java
@@ -0,0 +1,8 @@
+public class VolumeOfCylinder {
+    public static void main(String[] args) {
+        double radius = 3;
+        double height = 5;
+        double volume = 3.14 * radius * radius * height;
+        System.out.println(volume);
+    }
+}
