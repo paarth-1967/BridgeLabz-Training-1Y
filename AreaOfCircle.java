1b97bbfe0711cd921262a0471fbc8b0e2beb6f03
paarth-1967 <paarth30072006@gmail.com>
2026-01-12 21:21:04 +0530

    added: AreaOfCircle programming-constructs

---
+++ AreaOfCircle.java
@@ -0,0 +1,7 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 5;
+        double area = 3.14 * radius * radius;
+        System.out.println(area);
+    }
+}
