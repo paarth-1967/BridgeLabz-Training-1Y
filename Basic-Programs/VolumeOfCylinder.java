6aab3c5de60354ca049589a45516b656e88679dd
paarth <paarth30072006@gmail.com>
2026-01-15 14:10:18 +0530

    Update VolumeOfCylinder.java

--- Basic-Programs/VolumeOfCylinder.java
+++ Basic-Programs/VolumeOfCylinder.java
@@ -1,8 +1,23 @@
-public class VolumeOfCylinder {
+import java.util.Scanner;
+
+public class CylinderVolume {
     public static void main(String[] args) {
-        double radius = 3;
-        double height = 5;
-        double volume = 3.14 * radius * radius * height;
-        System.out.println(volume);
+        Scanner sc = new Scanner(System.in);
+
+        // Input radius and height
+        System.out.print("Enter the radius of the cylinder: ");
+        double radius = sc.nextDouble();
+
+        System.out.print("Enter the height of the cylinder: ");
+        double height = sc.nextDouble();
+
+        // Calculate volume
+        double volume = Math.PI * radius * radius * height;
+
+        // Display result
+        System.out.println("The volume of the cylinder is: " + volume);
+
+        sc.close();
     }
 }
+
