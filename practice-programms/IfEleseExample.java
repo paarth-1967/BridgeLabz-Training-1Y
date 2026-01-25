46de4800163b6ba6b62bdbb500a0a0b03e64fb4e
paarth-1967 <paarth30072006@gmail.com>
2026-01-25 20:04:24 +0530

    added

---
+++ practice-programms/IfEleseExample.java
@@ -0,0 +1,21 @@
+//Example for if statement
+
+class IfEleseExample{
+public static void main(String args[])  // main method
+{
+
+ int age=16;
+ 
+ if(age >= 17)
+ {
+     
+     System.out.println("Eligible" );  // if block
+
+}
+
+else{
+         System.out.println("Not Eligible" );  // else block
+
+}
+}
+}
